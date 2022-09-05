4c754d94c193f03302890da8df93067deeed6ec2
Sami Sezgin <sami_sezgin@hotmail.com>
2022-09-05 23:17:55 +0300

    Implemented: - Entity Framework Core - Dependency Injection

---
+++ WebApi/DBOperations/DataGenerator.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace WebApi.DBOperations
+{
+    public class DataGenerator
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new BookStoreDbContext(serviceProvider.GetRequiredService<DbContextOptions<BookStoreDbContext>>()))
+            {
+                if (context.Books.Any())
+                {
+                    return;
+                }
+
+                context.Books.AddRange(
+                    new Book
+                    {
+                        Id = 1,
+                        Title = "Lean Startup", //personal growth
+                        GenreId = 1,
+                        PageCount = 200,
+                        PublishDate = new System.DateTime(2001, 6, 12)
+                    },
+                    new Book
+                    {
+                        Id = 2,
+                        Title = "Herland", //science fiction
+                        GenreId = 2,
+                        PageCount = 250,
+                        PublishDate = new System.DateTime(2020, 5, 13)
+                    },
+                    new Book
+                    {
+                        Id = 3,
+                        Title = "Lean Startup",
+                        GenreId = 2, // science fiction
+                        PageCount = 500,
+                        PublishDate = new System.DateTime(2006, 7, 10)
+                    }
+                );
+
+
+                context.SaveChanges();
+            }
+        }
+    }
+}
