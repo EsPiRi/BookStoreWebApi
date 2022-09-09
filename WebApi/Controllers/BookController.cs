3f2a88b2e99833f20e694f8c87a1fcb9d674ecf7
Sami Sezgin <sami_sezgin@hotmail.com>
2022-09-10 00:05:49 +0300

    Empty Commit

--- WebApi/Controllers/BookController.cs
+++ WebApi/Controllers/BookController.cs
@@ -58,7 +58,7 @@ namespace WebApi.Controllers
                 return BadRequest(ex.Message);
             }
 
-            return Ok();
+            return Ok(); 
         }
 
         [HttpPut("{id}")]
